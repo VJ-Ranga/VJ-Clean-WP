0374f4901064bf9896fa6a72f9331059d327daee
VJRanga <viraj.madurange@gmail.com>
2023-05-08 01:09:07 +0530

    2.0

--- clean-wp.php
+++ clean-wp.php
@@ -1,37 +1,31 @@
 <?php
 /**
- * Plugin Name: VJ Clean WP
- * Plugin URI: https://cloudycode.net/
+ * Plugin Name: Clean WP
+ * Plugin URI: https://yourwebsite.com/clean-wp
  * Description: Delete all posts, pages, and plugins with the click of a button.
- * Version: 1.0
- * Author: VJRanga
- * Author URI: https://cloudycode.net/
+ * Version: 2.0
+ * Author: Your Name
+ * Author URI: https://yourwebsite.com
  */
 
 // Add a button to the admin bar
-add_action('admin_bar_menu', 'vj_clean_wp_admin_bar_menu', 100);
-function vj_clean_wp_admin_bar_menu($wp_admin_bar) {
+add_action('admin_bar_menu', 'clean_wp_admin_bar_menu', 100);
+function clean_wp_admin_bar_menu($wp_admin_bar) {
     $args = array(
         'id' => 'clean-wp-button',
-        'title' => 'VJ Clean WP',
+        'title' => 'Clean WP',
         'href' => '#',
         'meta' => array(
             'class' => 'clean-wp-button',
-            'onclick' => 'vj_clean_wp();'
+            'onclick' => 'clean_wp(); return false;'
         )
     );
     $wp_admin_bar->add_node($args);
 }
 
-// Add JavaScript to handle the button click
-add_action('admin_enqueue_scripts', 'vj_clean_wp_enqueue_scripts');
-function vj_clean_wp_enqueue_scripts() {
-    wp_enqueue_script('clean-wp-script', plugin_dir_url(__FILE__) . 'clean-wp.js', array('jquery'), '1.0', true);
-}
-
 // Handle form submissions
-add_action('wp_ajax_vj_clean_wp', 'vj_clean_wp_ajax_handler');
-function vj_clean_wp_ajax_handler() {
+add_action('wp_ajax_clean_wp', 'clean_wp_ajax_handler');
+function clean_wp_ajax_handler() {
     // Delete all posts
     $args = array(
         'post_type' => 'post',
@@ -57,7 +51,7 @@ function vj_clean_wp_ajax_handler() {
     // Deactivate all plugins except for the clean-wp plugin
     $plugins = get_plugins();
     foreach ($plugins as $plugin_file => $plugin_data) {
-        if ($plugin_file != 'clean-wp/clean-wp.php') {
+        if ($plugin_file != 'clean-wp2/clean-wp.php') {
             deactivate_plugins($plugin_file);
         }
     }
@@ -66,25 +60,49 @@ function vj_clean_wp_ajax_handler() {
     $plugins = get_plugins();
     foreach ($plugins as $plugin_file => $plugin_data) {
         // Only delete plugins that are not active and not the Clean WP plugin
-        if (!is_plugin_active($plugin_file) && $plugin_file != 'clean-wp/clean-wp.php') {
+        if (!is_plugin_active($plugin_file) && $plugin_file != 'clean-wp2/clean-wp.php') {
             delete_plugins(array($plugin_file));
         }
     }
-
-    // Delete all themes
-    $all_themes = wp_get_themes();
-    foreach ( $all_themes as $theme_key => $theme ) {
-    if ( $theme->get_stylesheet() !== 'twentytwentythree' ) {
-        delete_theme( $theme_key );
-        }
-    }
-
+    
     // Return a success message
     wp_send_json_success('Clean WP completed successfully.');
 }
 
 // Auto refresh the page after cleanup
-add_action('wp_ajax_vj_clean_wp_refresh', 'vj_clean_wp_ajax_refresh_handler');
-function vj_clean_wp_ajax_refresh_handler() {
+add_action('wp_ajax_clean_wp_refresh', 'clean_wp_ajax_refresh_handler');
+add_action('wp_ajax_nopriv_clean_wp_refresh', 'clean_wp_ajax_refresh_handler');
+function clean_wp_ajax_refresh_handler() {
     wp_send_json_success('Refreshed page.');
 }
+
+// Debugging statement to check if headers have already been sent
+add_action('activated_plugin','clean_wp_check_headers_sent');
+function clean_wp_check_headers_sent(){
+    if(headers_sent()){
+        error_log('Headers already sent in Clean WP plugin');
+    }
+}
+
+?>
+
+<script>
+// Define the function to handle the Clean WP button click
+function clean_wp() {
+    if (confirm('Are you sure you want to delete all posts, pages, and plugins? This action cannot be undone.')) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', '<?php echo admin_url('admin-ajax.php'); ?>');
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        xhr.onload = function() {
+            if (xhr.status === 200) {
+                alert(JSON.parse(xhr.responseText).data);
+                location.reload();
+            }
+            else {
+                alert('Clean WP failed.');
+            }
+        };
+        xhr.send('action=clean_wp');
+    }
+}
+</script>
